697b93a366fba23c2caa90f558b1b7578abf00ab
Techy-ui <akash16260102@gmail.com>
2026-06-15 20:50:14 +0530

    .....

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hyperlog/', // 👈 Make sure this matches your exact GitHub repo name
+  base: './', 
 })
